db1f6ceae4d1f2cbdc264819f6a5b1d8c2cf3eae
Alyssa Chan <alyssa@whiletrue.codes>
2019-02-07 14:37:48 +0800

    update hash for register endpoint

--- server.js
+++ server.js
@@ -57,22 +57,30 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { name, email, password } = req.body
-  db('users')
-    .returning('*')
-    .insert({
-      name: name,
-      email: email,
-      joined: new Date()
+  const hash = bcrypt.hashSync(password)
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
     })
-    .then(user => {
-      res.json(user[0])
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+      return trx('users')
+        .returning('*')
+        .insert({
+          email: loginEmail[0],
+          name: name,
+          joined: new Date()
+        })
+        .then(user => {
+          res.json(user[0])
+        })
     })
-    .catch(err => res.status(400).json('Unable to register'))
-  // db('login')
-  //   .insert({
-  //     email: email,
-  //     hash: bcrypt.hashSync(password)
-  //   })
+    .then(trx.commit)
+    .catch(trx.rollback)
+  })
+  .catch(err => res.status(400).json(err))
 })
 
 app.get('/profile/:id', (req, res) => {
