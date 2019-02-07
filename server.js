14ecd9bd1defdab08db5716c316741a4de9e5c5a
Alyssa Chan <alyssa@whiletrue.codes>
2019-02-07 17:18:18 +0800

    setup environmental variable

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.get('/profile/:id', profile.handleProfileGet(db))
 app.put('/image', image.handleImage(db))
 app.post('/imageurl', image.handleApiCall)
 
-app.listen(3000, () => {
-  console.log('app is running on port 3000')
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`app is running on port ${PORT}`)
 })
