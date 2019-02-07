f6e9596db99da11865ea1901d7224a207ed8d69c
Alyssa Chan <alyssa@whiletrue.codes>
2019-02-07 16:30:39 +0800

    create image controller

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const knex = require('knex')
 const register = require('./controllers/register')
 const signin = require('./controllers/signin')
 const profile = require('./controllers/profile')
+const image = require('./controllers/image')
 
 const db = knex({
   client: 'pg',
@@ -21,22 +22,12 @@ const db = knex({
 
 app.use(cors())
 app.use(bodyParser.json())
+
 app.get('/', (req, res) => res.send('Face Recognition Brain'))
-
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
-
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
-
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
-
-app.put('/image', (req, res) => {
-  const { id } = req.body
-  db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => res.json(entries[0]))
-    .catch(err => res.status(400).json('unable to get entries'))
-})
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 
 app.listen(3000, () => {
   console.log('app is running on port 3000')
